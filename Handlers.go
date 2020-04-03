e04746880aa7e03eebae5020a679bb4f7a0d9bfb
AMIRmh <amir.haghollahi@gmail.com>
2020-04-03 16:17:23 +0430

    handlers added

---
+++ Handlers.go
@@ -0,0 +1,46 @@
+package shm
+
+import (
+	"net/http"
+	"time"
+)
+
+type registrationStruct struct {
+	ip   string
+	time time.Time
+}
+
+var (
+	Registers []registrationStruct
+)
+
+func getIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
+}
+
+func AddRegister(r *http.Request) {
+	reg := registrationStruct{ip: getIP(r), time: time.Now()}
+	Registers = append(Registers, reg)
+}
+
+func AutomaticRegistrationPrevention(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for index, reg := range Registers {
+			if getIP(r) == reg.ip {
+				if time.Now().Sub(reg.time).Minutes() < 15 {
+					w.WriteHeader(406)
+					return
+				} else {
+					next.ServeHTTP(w, r)
+					Registers = append(Registers[:index],
+						Registers[index+1:]...)
+					break
+				}
+			}
+		}
+	})
+}
