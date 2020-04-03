af164b1794d8991ddcb5f66291a0e878b2c6a049
AMIRmh <amir.haghollahi@gmail.com>
2020-04-03 15:39:32 +0430

    main in progress

--- test/main.go
+++ test/main.go
@@ -1,6 +1,38 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	shm "secure-http-management"
+)
+
+func loginHanlder(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	checkErr(err)
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	err = shm.VerifyPassword(password, username)
+	if err != nil {
+		if err.Error() == shm.ErrPassWrong.ErrorMsg ||
+			err.Error() == shm.ErrUserNotFound.ErrorMsg {
+			w.WriteHeader(401)
+			_, err = w.Write([]byte("username or password is wrong"))
+			checkErr(err)
+			return
+		}
+	}
+
+}
+
 func main() {
+	//http.HandleFunc("/login", loginHanlder)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/login", loginHanlder)
+
+	log.Fatal(http.ListenAndServeTLS(":443",
+		"server.crt", "server.key", mux))
 
 }
 
